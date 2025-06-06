7d36946a9fa460e3889ba474322e62aa84addb1c
user2510s <riangameplay124@gmail.com>
2025-06-06 18:21:32 -0300

    lembret

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,22 @@
-import fastify from "fastify"
-import { routes } from "./routes"
-import cors from "@fastify/cors"
+import fastify from "fastify";
+import { routes } from "./routes";
+import cors from "@fastify/cors";
 
-export const app = fastify()
-app.register(cors, {})
+export const app = fastify();
+app.register(cors, {});
 
-app.register(routes)
+app.register(routes);
 
-app.listen({
+app.listen(
+  { 
+    // host: '0.0.0.0', 
     port: 3333,
-},(err, address) =>{
-    if(err){
-        console.log("error")
-    }else{
-        console.log("serve iniciado")
+  },
+  (err, address) => {
+    if (err) {
+      console.log("error");
+    } else {
+      console.log("serve iniciado");
     }
-
-}
-)
+  }
+);
